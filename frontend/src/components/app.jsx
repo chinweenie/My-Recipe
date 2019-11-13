67d0ceb005b3ebb80591f2a12482596e758fdbb0
Roger Kiew <yuichiu416@gmail.com>
2019-11-13 01:22:39 -0600

    made a template, can work on more css

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,13 +3,15 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch, Route } from 'react-router-dom';
 import MainPage from './main/main_page';
 import Modal from './modal/modal';
+import Profile from './profile/profile';
 
 const App = () => (
     <div>
         <Modal/>
 
         <Switch>
-            <Route exact path="/" component={MainPage} /> 
+            <Route exact path="/" component={MainPage} />
+            <Route exact path="/profile" component={Profile} /> 
             {/* <AuthRoute exact path="/login" component={LoginFormContainer}/>
             <AuthRoute exact path="/register" component={SignupFormContainer}/>  */}
         </Switch>
---
+++ frontend/src/components/profile/profile.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <div className="profile">
+                <div className="profile-header">
+                        <div className="profile-picture">
+
+                        </div>
+                        <div className="profile-info">
+                            <h2>User Name</h2>
+                            <p>tell us about you here </p>
+                            <div className="profile-social-media">
+
+                            </div>
+                        </div>
+                </div>
+                <div className="profile-tabs">
+                    <Link to="/profile/userid">SAVED RECIPES</Link>
+                    <Link to="/settings/preferences">PREFERENCES</Link>
+                    <Link to="/settings">SETTINGS</Link>
+                </div>
+                <input className="profile-search" placeholder="Search My Yums" />
+                <h2>All Yums</h2>
+                <div className="profile-yums">
+
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                </div>
+                <h2>collections</h2>
+                <div className="profile-collections">
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                    <div className="profile-yum">
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
