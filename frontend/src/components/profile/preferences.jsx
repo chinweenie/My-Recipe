c0c58d959beb7a3531de2e9682b5bce776ce3bbd
Roger Kiew <yuichiu416@gmail.com>
2019-11-14 15:39:25 -0600

    preference page with nice css animation

--- frontend/src/components/profile/preferences.jsx
+++ frontend/src/components/profile/preferences.jsx
@@ -5,10 +5,39 @@ export default class Preferences extends Component {
         return (
             <div className="preference">
                 <div className="preference-row">
-                    <h1>Diets</h1>
-                    <p>Select from the diets besow and we will only show you recipes that match</p>
+                    <h2>Diets</h2>
+                    <p>Select from the diets besow and we will only show you recipes that match.</p>
                     <div className="preference-row-item">
-                        ADD DIETS
+                        <div>+
+                            <p>ADD DIETS</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="preference-row">
+                    <h2>Allergies</h2>
+                    <p>Select from the allergies below and we will only show you recipes that match.</p>
+                    <div className="preference-row-item">
+                        <div>+
+                            <p>ADD ALLERGIES</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="preference-row">
+                    <h2>Disliked Ingredients</h2>
+                    <p>Add disliked ingredients to avoid recipes that include them.</p>
+                    <div className="preference-row-item">
+                        <div>+
+                            <p>ADD DISLIKED INGREDIENTS</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="preference-row">
+                    <h2>Favorite Cuisines</h2>
+                    <p>Select your favorite cuisines to see more of what you like.</p>
+                    <div className="preference-row-item">
+                        <div>+
+                            <p>ADD DIETS</p>
+                        </div>
                     </div>
                 </div>
                 
