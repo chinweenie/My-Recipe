00949cfb11f8fb1986b1ed5516b7bf107e6bf986
Roger Kiew <yuichiu416@gmail.com>
2019-11-15 00:06:56 -0600

    added settings layout and two buttons

--- frontend/src/components/profile/settings.jsx
+++ frontend/src/components/profile/settings.jsx
@@ -3,8 +3,49 @@ import React, { Component } from 'react'
 export default class Settings extends Component {
     render() {
         return (
-            <div className="">
-                I am settings page
+            <div className="settings">
+                <div className="settings-flex-item">
+                    <h2>General Information</h2>
+                    <div className="flex-row">
+                        <div className="preference-row">
+                            <div className="preference-row-item">
+                                <div>+
+                            <p>GENDER</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="preference-row">
+                            <div className="preference-row-item">
+                                <div>+
+                            <p>SKILLS</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="preference-row">
+                            <div className="preference-row-item">
+                                <div>+
+                            <p>HOUSEHOLD SIZE</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="settings-flex-item">
+                    <h2>Account Settings</h2>
+                </div>
+                <div className="settings-flex-item">
+                    <h2>Subscription</h2>
+                </div>
+                <div className="settings-flex-item">
+                    <h2>Social Settings</h2>
+                </div>
+                <div className="settings-flex-item">
+                    <h2>Email Preferences</h2>
+                </div>
+                <div className="flex-row">
+                    <div className="confirm-btn">Save</div>
+                    <div className="cancel-btn">Unsubscribe from all</div>
+                </div>
             </div>
         )
     }
